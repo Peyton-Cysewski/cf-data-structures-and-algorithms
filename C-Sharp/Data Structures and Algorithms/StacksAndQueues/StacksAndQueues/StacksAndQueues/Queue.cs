667fe9a38e425120681c2d4ffbc0704d4b86c129
Peyton Cysewski <pcysewski12@gmail.com>
2020-07-20 06:51:47 -0700

    completed all methods and tests

--- C-Sharp/Data Structures and Algorithms/StacksAndQueues/StacksAndQueues/StacksAndQueues/Queue.cs
+++ C-Sharp/Data Structures and Algorithms/StacksAndQueues/StacksAndQueues/StacksAndQueues/Queue.cs
@@ -1,10 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace StacksAndQueues
 {
-    class Queue
+    public class Queue
     {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public Queue()
+        {
+            Rear = Front;
+        }
+
+        public void Enqueue(int value)
+        {
+            Node temp = new Node(value);
+            if (Rear == null)
+            {
+                Front = temp;
+                Rear = temp;
+            }
+            else
+            {
+                Rear.Next = temp;
+                Rear = temp;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (Front == null)
+                throw new NullReferenceException();
+            else if (Front.Next == null)
+            {
+                int temp = Front.Value;
+                Front = null;
+                Rear = Front;
+                return temp;
+            }
+            else
+            {
+                int temp = Front.Value;
+                Front = Front.Next;
+                return temp;
+            }
+        }
+
+        public int Peek()
+        {
+            return Front.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            if (Front == null) return true;
+            else return false;
+        }
     }
 }
