0853bf7f01dc9a70668d71929f7744bbf9d92123
Peyton Cysewski <pcysewski12@gmail.com>
2020-07-21 07:20:45 -0700

    finished code challenge

---
+++ C-Sharp/Code Challenges/QueueWithStacks/QueueWithStacks/QueueWithStacks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QueueWithStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFakeQueue();
+        }
+
+        static void RunFakeQueue()
+        {
+            PseudoQueue fake = new PseudoQueue();
+            fake.Enqueue(1);
+            fake.Enqueue(2);
+            fake.Enqueue(3);
+            fake.Enqueue(4);
+            fake.Enqueue(5);
+            int[] emptied = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                emptied[i] = fake.Dequeue();
+            }
+            foreach (int num in emptied)
+            {
+                Console.WriteLine($"{num}");
+            }
+        }
+    }
+}
--- C-Sharp/Code Challenges/README.md
+++ C-Sharp/Code Challenges/README.md
@@ -5,3 +5,4 @@ This directory holds all of my code challenges that are done in C#.
 ## Table of Contents
 - [Array Shift](./ArrayShift)
 - [Zipped Linked List](./LinkedListZip)
+- [Queue With Stacks](./QueueWithStacks)
