8ff10bbf7433a7526a8e8cb6284b6f8a22697a19
Peyton Cysewski <pcysewski12@gmail.com>
2020-07-26 16:32:14 -0700

    added all tests and methods for base requirements

--- C-Sharp/Data Structures and Algorithms/Trees/Trees/Trees/Tree.cs
+++ C-Sharp/Data Structures and Algorithms/Trees/Trees/Trees/Tree.cs
@@ -1,8 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Trees
 {
-    public class Class1
+    public class Tree<T>
     {
+        public Node<T> Root { get; set; }
+        public Tree() { }
+        public Tree(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            Root = node;
+        }
+        public T[] PreOrder(Node<T> root)
+        {
+            List<T> list = new List<T>();
+            PreOrder(list, root);
+            return list.ToArray();
+        }
+        private void PreOrder(List<T> list, Node<T> node)
+        {
+            list.Add(node.Value);
+            if (node.LeftChild != null)
+            {
+                PreOrder(list, node.LeftChild);
+            }
+            if (node.RightChild != null)
+            {
+                PreOrder(list, node.RightChild);
+            }
+        }
+
+        public T[] InOrder(Node<T> root)
+        {
+            List<T> list = new List<T>();
+            InOrder(list, root);
+            return list.ToArray();
+        }
+        private void InOrder(List<T> list, Node<T> node)
+        {
+            if (node.LeftChild != null)
+            {
+                InOrder(list, node.LeftChild);
+            }
+            list.Add(node.Value);
+            if (node.RightChild != null)
+            {
+                InOrder(list, node.RightChild);
+            }
+        }
+
+        public T[] PostOrder(Node<T> root)
+        {
+            List<T> list = new List<T>();
+            PostOrder(list, root);
+            return list.ToArray();
+        }
+        private void PostOrder(List<T> list, Node<T> node)
+        {
+            if (node.LeftChild != null)
+            {
+                PostOrder(list, node.LeftChild);
+            }
+            if (node.RightChild != null)
+            {
+                PostOrder(list, node.RightChild);
+            }
+            list.Add(node.Value);
+        }
     }
 }
