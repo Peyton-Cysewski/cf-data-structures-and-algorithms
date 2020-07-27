e21938af4a514d835403f1210b403cc4b578ca17
Peyton Cysewski <pcysewski12@gmail.com>
2020-07-26 17:27:59 -0700

    added method comments

--- C-Sharp/Data Structures and Algorithms/Trees/Trees/Trees/Tree.cs
+++ C-Sharp/Data Structures and Algorithms/Trees/Trees/Trees/Tree.cs
@@ -12,12 +12,25 @@ namespace Trees
             Node<T> node = new Node<T>(value);
             Root = node;
         }
+        /// PreOrder
+        #region
+        /// <summary>
+        /// Returns an array of the values contained in the tree ordered by the recursive pattern: root, right child, left child
+        /// </summary>
+        /// <param name="root">The node that will act as the upper root for the tree traversal</param>
+        /// <returns>Ordered array of the tree's values starting from the given root</returns>
         public T[] PreOrder(Node<T> root)
         {
             List<T> list = new List<T>();
             PreOrder(list, root);
             return list.ToArray();
         }
+
+        /// <summary>
+        /// Indirect recursive method associated with its overload counterpart
+        /// </summary>
+        /// <param name="list">List of ordered values that is added to with each recursion</param>
+        /// <param name="node">Current root node</param>
         private void PreOrder(List<T> list, Node<T> node)
         {
             list.Add(node.Value);
@@ -30,13 +43,27 @@ namespace Trees
                 PreOrder(list, node.RightChild);
             }
         }
+        #endregion
 
+        /// InOrder
+        #region
+        /// <summary>
+        /// Returns an array of the values contained in the tree ordered by the recursive pattern: right child, root, left child
+        /// </summary>
+        /// <param name="root">The node that will act as the upper root for the tree traversal</param>
+        /// <returns>Ordered array of the tree's values starting from the given root</returns>
         public T[] InOrder(Node<T> root)
         {
             List<T> list = new List<T>();
             InOrder(list, root);
             return list.ToArray();
         }
+
+        /// <summary>
+        /// Indirect recursive method associated with its overload counterpart
+        /// </summary>
+        /// <param name="list">List of ordered values that is added to with each recursion</param>
+        /// <param name="node">Current root node</param>
         private void InOrder(List<T> list, Node<T> node)
         {
             if (node.LeftChild != null)
@@ -49,13 +76,27 @@ namespace Trees
                 InOrder(list, node.RightChild);
             }
         }
+        #endregion
 
+        /// PostOrder
+        #region
+        /// <summary>
+        /// Returns an array of the values contained in the tree ordered by the recursive pattern: right child, left child, root
+        /// </summary>
+        /// <param name="root">The node that will act as the upper root for the tree traversal</param>
+        /// <returns>Ordered array of the tree's values starting from the given root</returns>
         public T[] PostOrder(Node<T> root)
         {
             List<T> list = new List<T>();
             PostOrder(list, root);
             return list.ToArray();
         }
+
+        /// <summary>
+        /// Indirect recursive method associated with its overload counterpart
+        /// </summary>
+        /// <param name="list">List of ordered values that is added to with each recursion</param>
+        /// <param name="node">Current root node</param>
         private void PostOrder(List<T> list, Node<T> node)
         {
             if (node.LeftChild != null)
@@ -68,5 +109,6 @@ namespace Trees
             }
             list.Add(node.Value);
         }
+        #endregion
     }
 }
