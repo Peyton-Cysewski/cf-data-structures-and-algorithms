f693655b0ea6aac51722d80165ab91ab2c610605
Peyton Cysewski <pcysewski12@gmail.com>
2020-07-26 17:44:47 -0700

    fixed issue with Node class being fully empty for some reason

--- C-Sharp/Data Structures and Algorithms/Trees/Trees/Trees/Node.cs
+++ C-Sharp/Data Structures and Algorithms/Trees/Trees/Trees/Node.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Trees
 {
-    class Node
+    public class Node<T>
     {
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+        public T Value { get; set; }
+        public Node(T value)
+        {
+            Value = value;
+        }
     }
 }
