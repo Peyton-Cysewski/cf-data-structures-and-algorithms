190582ccbe1df2ee194594c40f598e30b8467150
Peyton Cysewski <pcysewski12@gmail.com>
2020-07-29 12:11:06 -0700

    added findmaxvalue method and test

--- C-Sharp/Data Structures and Algorithms/Trees/Trees/TreeTests/UnitTest1.cs
+++ C-Sharp/Data Structures and Algorithms/Trees/Trees/TreeTests/UnitTest1.cs
@@ -132,6 +132,32 @@ namespace TreeTests
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void FindMaximumValueWorks()
+        {
+            // Assign
+            Tree<int> tree = new Tree<int>();
+            Node<int> root = new Node<int>(1);
+            Node<int> left = new Node<int>(2);
+            Node<int> right = new Node<int>(3);
+            Node<int> leftLeft = new Node<int>(4);
+            Node<int> leftRight = new Node<int>(5);
+            Node<int> rightLeft = new Node<int>(6);
+            Node<int> rightRight = new Node<int>(7);
+            left.LeftChild = leftLeft;
+            left.RightChild = leftRight;
+            right.LeftChild = rightLeft;
+            right.RightChild = rightRight;
+            root.LeftChild = left;
+            root.RightChild = right;
+            tree.Root = root;
+            // Act
+            int expected = 7;
+            int actual = tree.FindMaximumValue();
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
     }
     public class BinarySearchTrees
     {
