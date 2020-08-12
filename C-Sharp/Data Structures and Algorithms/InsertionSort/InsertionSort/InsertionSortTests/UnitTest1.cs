d5031c4e6bed9a247598865c0924d747d27a0585
Peyton Cysewski <pcysewski12@gmail.com>
2020-08-11 22:38:59 -0700

    fixed insertion sort file

--- C-Sharp/Data Structures and Algorithms/InsertionSort/InsertionSort/InsertionSortTests/UnitTest1.cs
+++ C-Sharp/Data Structures and Algorithms/InsertionSort/InsertionSort/InsertionSortTests/UnitTest1.cs
@@ -29,5 +29,53 @@ namespace InsertionSortTests
             // Assert
             Assert.Equal(expected, array);
         }
+
+        [Fact]
+        public void InsertionSortWorksWithRepeats()
+        {
+            // Assign
+            int[] array = { 8, 5, 5, 8, 9, 5 };
+            // Act
+            Sort(array);
+            int[] expected = { 5, 5, 5, 8, 8, 9 };
+            // Assert
+            Assert.Equal(expected, array);
+        }
+
+        [Fact]
+        public void InsertionSortWorksWithAnInvertedArray()
+        {
+            // Assign
+            int[] array = { 6, 5, 4, 3, 2, 1 };
+            // Act
+            Sort(array);
+            int[] expected = { 1, 2, 3, 4, 5, 6 };
+            // Assert
+            Assert.Equal(expected, array);
+        }
+
+        [Fact]
+        public void InsertionSortWorksWithAPreSortedArray()
+        {
+            // Assign
+            int[] array = { 2, 5, 6, 9, 12, 42 };
+            // Act
+            Sort(array);
+            int[] expected = { 2, 5, 6, 9, 12, 42 };
+            // Assert
+            Assert.Equal(expected, array);
+        }
+
+        [Fact]
+        public void InsertionSortReferencesTheSameArray()
+        {
+            // Assign
+            int[] array = { 2, 5, 6, 9, 12, 42 };
+            var reference = array;
+            // Act
+            Sort(array);
+            // Assert
+            Assert.Same(reference, array);
+        }
     }
 }
