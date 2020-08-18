6c27d828f8b0792210dadef762df9fd7debb7fac
Peyton Cysewski <pcysewski12@gmail.com>
2020-08-17 21:01:52 -0700

    please save

--- C-Sharp/Code Challenges/RepeatedWord/RepeatedWord/RepeatedWord/RepeatedWord.cs
+++ C-Sharp/Code Challenges/RepeatedWord/RepeatedWord/RepeatedWord/RepeatedWord.cs
@@ -3,24 +3,21 @@ using System.Collections;
 
 namespace RepeatedWord
 {
-    public class RepeatWords
+    public class RepeatedWord
     {
         public static string CheckRepeat(string language)
         {
-            string[] words = language.Split(',', ' ', '-', '.');
+            string[] words = language.Split('.', ' ', '-', '.');
             Hashtable exists = new Hashtable();
             for (int i = 0; i < words.Length; i++)
             {
-                if (words[i] != "")
+                if (exists.ContainsKey(words[i].ToLower()))
                 {
-                    if (exists.ContainsKey(words[i].ToLower()))
-                    {
-                        return words[i];
-                    }
-                    else
-                    {
-                        exists.Add(words[i].ToLower(), null);
-                    }
+                    return words[i];
+                }
+                else
+                {
+                    exists.Add(words[i].ToLower(), null);
                 }
             }
             return "no repeats";
