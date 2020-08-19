527b1a4a7e84b5863309f1d379bcd0ee3088c201
Peyton Cysewski <pcysewski12@gmail.com>
2020-08-19 08:37:48 -0700

    added code comments

--- C-Sharp/Code Challenges/TreeIntersection/TreeIntersection/TreeIntersection/Class1.cs
+++ C-Sharp/Code Challenges/TreeIntersection/TreeIntersection/TreeIntersection/Class1.cs
@@ -7,6 +7,12 @@ namespace TreeIntersectionChallenge
 {
     public static class TreeIntersection
     {
+        /// <summary>
+        /// Takes in two trees and returns a list of any values that are shared between the two.
+        /// </summary>
+        /// <param name="tree1">Tree 1 object.</param>
+        /// <param name="tree2">Tree 2 object.</param>
+        /// <returns>List of shared values.</returns>
         public static List<int> FindIntersections(Tree<int> tree1, Tree<int> tree2)
         {
             HashSet<int> hashset = new HashSet<int>();
@@ -33,6 +39,11 @@ namespace TreeIntersectionChallenge
             return intersections;
         }
 
+        /// <summary>
+        /// Traverses an tree in-order and returns a list of all the tree's values.
+        /// </summary>
+        /// <param name="node">Root tree node.</param>
+        /// <returns>List of values.</returns>
         private static List<int> Traverse(Node<int> node)
         {
             List<int> values = new List<int>();
@@ -40,6 +51,11 @@ namespace TreeIntersectionChallenge
             return values;
         }
 
+        /// <summary>
+        /// Recurssive elper function for the Traversal method to sort in-order.
+        /// </summary>
+        /// <param name="node">Input node, starts as the root.</param>
+        /// <param name="list">List that keeps track of all tree values.</param>
         private static void TraverseHelper(Node<int> node, List<int> list)
         {
             if (node.LeftChild != null)
