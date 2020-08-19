3294e3685c0383bfc37ab8e75906039dfc145d33
Peyton Cysewski <pcysewski12@gmail.com>
2020-08-19 07:31:51 -0700

    weird error in hashtable class

--- C-Sharp/Data Structures and Algorithms/Hashtable/Hashtable/Hashtable/Hashtable.cs
+++ C-Sharp/Data Structures and Algorithms/Hashtable/Hashtable/Hashtable/Hashtable.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace HashtableLibrary
@@ -12,6 +11,12 @@ namespace HashtableLibrary
             Map = new LinkedList<LinkedListNode<Pair<T>>>[size];
         }
 
+        /// <summary>
+        /// Uses the given Key and hashes an index for the Hashtable.
+        /// </summary>
+        /// <param name="key">Key to be hashed.</param>
+        /// <param name="arraySize">Size of the Hashtable.</param>
+        /// <returns></returns>
         private int Hash(string key, int arraySize)
         {
             int val = 0;
@@ -23,6 +28,11 @@ namespace HashtableLibrary
             return index;
         }
 
+        /// <summary>
+        /// Adds a Key Value pair to the Hashtable.
+        /// </summary>
+        /// <param name="key">Pair Key.</param>
+        /// <param name="value">Pair Value.</param>
         public void Add(string key, T value)
         {
             int index = Hash(key, Map.Length);
@@ -30,11 +40,16 @@ namespace HashtableLibrary
             LinkedListNode<Pair<T>> node = new LinkedListNode<Pair<T>>(pair);
             if (Map[index] is null)
             {
-                Map[index] = new LinkedList<LinkedListNode<Pair<T>>>;
+                Map[index] = new LinkedList<LinkedListNode<Pair<T>>>();
             }
             Map[index].AddLast(node);
         }
 
+        /// <summary>
+        /// Takes in a Key and returns the associated Value.
+        /// </summary>
+        /// <param name="key">Target Key.</param>
+        /// <returns>Value associated with Key. If no Value exists, then default value is returned.</returns>
         public T Get(string key)
         {
             int index = Hash(key, Map.Length);
@@ -48,6 +63,26 @@ namespace HashtableLibrary
                 current = current.Next;
             }
             return default;
+        }
+
+        /// <summary>
+        /// Checks whether a Key Value pair exists in the Hashtable.
+        /// </summary>
+        /// <param name="key">Target Key.</param>
+        /// <returns>True if it exists in the the Hashtable, false if it does not.</returns>
+        public bool Contains(string key)
+        {
+            int index = Hash(key, Map.Length);
+            var current = Map[index].First;
+            while (current != null)
+            {
+                if (current.Value.Value.Key == key)
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
         }
     }
 }
--- C-Sharp/Data Structures and Algorithms/Hashtable/Hashtable/HashtableTests/UnitTest1.cs
+++ C-Sharp/Data Structures and Algorithms/Hashtable/Hashtable/HashtableTests/UnitTest1.cs
@@ -1,14 +1,61 @@
 using System;
 using Xunit;
+using HashtableLibrary;
 
 namespace HashtableTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void HashtableAddAndGetWorks()
         {
+            // Assign
+            Hashtable<int> hashtable = new Hashtable<int>(16);
+            // Act
+            hashtable.Add("Test", 10);
+            int actual = hashtable.Get("Test");
+            int expected = 10;
+            // Assert
+            Assert.Equal(expected, actual);
+        }
 
+        [Fact]
+        public void HashtableWorksWithCollisions()
+        {
+            // Assign
+            Hashtable<int> hashtable = new Hashtable<int>(1);
+            // Act
+            hashtable.Add("Test", 10);
+            hashtable.Add("Test2", 11);
+            hashtable.Add("Test3", 12);
+            int actual = hashtable.Get("Test3");
+            int expected = 12;
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void HashtableContainsWorks()
+        {
+            // Assign
+            Hashtable<int> hashtable = new Hashtable<int>(16);
+            // Act
+            hashtable.Add("Test", 10);
+            bool result = hashtable.Contains("Test");
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HashtableContainsReturnsFalse()
+        {
+            // Assign
+            Hashtable<int> hashtable = new Hashtable<int>(16);
+            // Act
+            hashtable.Add("Test", 10);
+            bool result = hashtable.Contains("Not Test");
+            // Assert
+            Assert.False(result);
         }
     }
 }
